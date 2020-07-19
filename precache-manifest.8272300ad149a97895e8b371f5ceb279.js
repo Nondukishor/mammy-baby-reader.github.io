04e59559ba6ab8bab769d8f1a644f06905aa735e
Nodukishor <nipuchakraborty86@hotmail.com>
2020-07-20 02:34:42 +0600

    Updates

---
+++ precache-manifest.8272300ad149a97895e8b371f5ceb279.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb1f8138101127ea13627330196cc1b9",
+    "url": "/mammy-baby-reader.github.io/index.html"
+  },
+  {
+    "revision": "9cf4fc159ea4b4219834",
+    "url": "/mammy-baby-reader.github.io/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "d0c06107de0472a78fa2",
+    "url": "/mammy-baby-reader.github.io/static/css/main.f216e56d.chunk.css"
+  },
+  {
+    "revision": "9cf4fc159ea4b4219834",
+    "url": "/mammy-baby-reader.github.io/static/js/2.90acc010.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/mammy-baby-reader.github.io/static/js/2.90acc010.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0c06107de0472a78fa2",
+    "url": "/mammy-baby-reader.github.io/static/js/main.d602c8df.chunk.js"
+  },
+  {
+    "revision": "2dde921db57e35b52462",
+    "url": "/mammy-baby-reader.github.io/static/js/runtime-main.acda8179.js"
+  }
+]);
